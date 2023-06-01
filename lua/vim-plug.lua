d013ace8e17e801d92f9a77859faef988bc73804
Psh <dev@mtbdx.com>
2023-06-02 01:19:23 +0900

    Install hop

--- lua/vim-plug.lua
+++ lua/vim-plug.lua
@@ -17,6 +17,8 @@ Plug 'nvim-lua/plenary.nvim' -- telescope dependency
 Plug('nvim-telescope/telescope.nvim', { tag = '*' })
 Plug('nvim-telescope/telescope-fzf-native.nvim', { ['do'] = 'make' })
 
+Plug 'phaazon/hop.nvim'
+
 Plug 'mbbill/undotree'
 Plug 'ThePrimeagen/vim-be-good'
 
