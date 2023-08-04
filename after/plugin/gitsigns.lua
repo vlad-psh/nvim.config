40e2cf357ffff8a3d08a08017af7a16c9b0ed470
Psh <dev@mtbdx.com>
2023-08-04 10:15:37 +0900

    Remap fugitive/gitsigns actions

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -1,1 +1,45 @@
-require('gitsigns').setup()
+require('gitsigns').setup{
+  on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    -- Navigation
+    -- map('n', ']c', function()
+    --   if vim.wo.diff then return ']c' end
+    --   vim.schedule(function() gs.next_hunk() end)
+    --   return '<Ignore>'
+    -- end, {expr=true})
+
+    -- map('n', '[c', function()
+    --   if vim.wo.diff then return '[c' end
+    --   vim.schedule(function() gs.prev_hunk() end)
+    --   return '<Ignore>'
+    -- end, {expr=true})
+
+    -- Actions
+    map('n', '<leader>gs', gs.stage_hunk)
+    map('n', '<leader>gr', gs.reset_hunk)
+    map('v', '<leader>gs', function() gs.stage_hunk {vim.fn.line("."), vim.fn.line("v")} end)
+    map('v', '<leader>gr', function() gs.reset_hunk {vim.fn.line("."), vim.fn.line("v")} end)
+    map('n', '<leader>gu', gs.undo_stage_hunk)
+
+    map('n', '<leader>gS', gs.stage_buffer)
+    map('n', '<leader>gR', gs.reset_buffer)
+    -- map('n', '<leader>hd', gs.diffthis)
+    -- map('n', '<leader>hD', function() gs.diffthis('~') end)
+
+    -- '<leader>h' git non-destructive actions
+    map('n', '<leader>hb', function() gs.blame_line{full=true} end)
+    -- map('n', '<leader>hb', gs.toggle_current_line_blame)
+    map('n', '<leader>hx', gs.toggle_deleted)
+    map('n', '<leader>hh', gs.preview_hunk)
+
+    -- Text object
+    -- map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+  end
+}
