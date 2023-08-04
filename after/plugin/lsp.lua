bbd6718ca870ca0ce679bd453ebe0652fe6d2aae
Psh <dev@mtbdx.com>
2023-08-04 10:15:40 +0900

    Add which-key + Configure descriptions + Remap

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -12,7 +12,6 @@ lsp.ensure_installed({
 -- Fix Undefined global 'vim'
 lsp.nvim_workspace()
 
-
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
@@ -40,18 +39,20 @@ lsp.set_preferences({
 })
 
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+  local function opts(desc)
+    return { buffer = bufnr, remap = false, desc = desc }
+  end
 
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts('Go to definition'))
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts('Show hover'))
+  vim.keymap.set("n", "<leader>ls", function() vim.lsp.buf.workspace_symbol() end, opts('List all workspace symbols'))
+  vim.keymap.set("n", "<leader>ld", function() vim.diagnostic.open_float() end, opts('Open diagnostics'))
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts('Go to previous diagnostic'))
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts('Go to next diagnostic'))
+  vim.keymap.set("n", "<leader>la", function() vim.lsp.buf.code_action() end, opts('Select a code action'))
+  vim.keymap.set("n", "<leader>lr", function() vim.lsp.buf.references() end, opts('List all references'))
+  vim.keymap.set("n", "<leader>lm", function() vim.lsp.buf.rename() end, opts('Rename all references'))
+  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts('Show signature help'))
 end)
 
 lsp.setup()
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,15 +1,15 @@
 local builtin = require('telescope.builtin')
 
 -- Default
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fs', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
+vim.keymap.set('n', '<leader>fs', builtin.live_grep, { desc = 'Grep string' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'List buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Find vim help' })
 
-vim.keymap.set('n', '<leader>fc', builtin.keymaps, {})
-vim.keymap.set('n', '<leader>fy', builtin.registers, {})
+vim.keymap.set('n', '<leader>fc', builtin.keymaps, { desc = 'Find keymaps' })
+vim.keymap.set('n', '<leader>fy', builtin.registers, { desc = 'List registers' })
 
-vim.keymap.set('n', '<leader>fif', builtin.git_files, {})
-vim.keymap.set('n', '<leader>fib', builtin.git_branches, {})
-vim.keymap.set('n', '<leader>fic', builtin.git_commits, {})
-vim.keymap.set('n', '<leader>fis', builtin.git_status, {})
+vim.keymap.set('n', '<leader>fif', builtin.git_files, { desc = 'Find git ls-files' })
+vim.keymap.set('n', '<leader>fib', builtin.git_branches, { desc = 'Git branches' })
+vim.keymap.set('n', '<leader>fic', builtin.git_commits, { desc = 'Git commits' })
+vim.keymap.set('n', '<leader>fis', builtin.git_status, { desc = 'Git status/stage' })
