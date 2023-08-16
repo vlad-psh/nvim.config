e53a6a49b7e5c8cd426eaa46dd58207a5d97babd
Psh <dev@mtbdx.com>
2023-08-16 12:55:51 +0900

    Lualine: Display full relative path; move diagnostics

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -3,12 +3,34 @@ require('lualine').setup {
     icons_enabled = false,
     theme = 'auto',
     globalstatus = false,
+    component_separators = { left = '⟩', right = '⟨' },
   },
   sections = {
     lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
-    lualine_x = {'filetype'},
+    lualine_b = {'branch', 'diff'},
+    lualine_c = {
+      {
+        'filename',
+        file_status = true,
+        newfile_status = false,
+        -- 0: Just the filename
+        -- 1: Relative path
+        -- 2: Absolute path
+        -- 3: Absolute path, with tilde as the home directory
+        -- 4: Filename and parent dir, with tilde as the home directory
+        path = 1,
+        -- Shortens path to leave 40 spaces in the window
+        -- for other components. (terrible name, any suggestions?)
+        shorting_target = 40,
+        symbols = {
+          modified = '[+]',
+          readonly = '[RO]',
+          unnamed = '[No Name]',
+          newfile = '[NEW]',
+        }
+      },
+    },
+    lualine_x = {'filetype', 'diagnostics'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
   },
