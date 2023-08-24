7c1fc6e57a636f42af9388a89bd5cd3b8dead25c
Psh <dev@mtbdx.com>
2023-08-24 13:55:38 +0900

    Telescope: 100% size + straight borders + show branches keymap

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,15 @@
+require('telescope').setup{
+  defaults = {
+    border = true,
+    borderchars = { "─", "│", "─", " ", "─", "┬", "┴", "─" },
+    layout_strategy = 'flex',
+    layout_config = {
+      height = 1000, -- Doesn't accepts 1.0 as 100% height
+      width = 1000,
+      prompt_position = 'bottom',
+    },
+  },
+}
 local builtin = require('telescope.builtin')
 
 -- For fzf configuration options see:
@@ -8,6 +20,6 @@ require('telescope').load_extension('fzf')
 vim.keymap.set('n', '<leader>/c', builtin.keymaps, { desc = 'Find keymaps' })
 vim.keymap.set('n', '<leader>/y', builtin.registers, { desc = 'List registers' })
 -- vim.keymap.set('n', '<leader>fif', builtin.git_files, { desc = 'Find git ls-files' })
--- vim.keymap.set('n', '<leader>fib', builtin.git_branches, { desc = 'Git branches' })
+vim.keymap.set('n', '<C-b>', builtin.git_branches, { desc = 'Git branches' })
 -- vim.keymap.set('n', '<leader>fic', builtin.git_commits, { desc = 'Git commits' })
 -- vim.keymap.set('n', '<leader>fis', builtin.git_status, { desc = 'Git status/stage' })
